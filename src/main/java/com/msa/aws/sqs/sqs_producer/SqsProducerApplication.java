8ffcf69c9f1a18ba38dc30950496324de57c539e
Misaki.Fujishiro <Misaki.Fujishiro@nttdata.com>
2023-05-28 11:45:49 +0900

    mod appspec ec2-users to ec2-user

--- src/main/java/com/msa/aws/sqs/sqs_producer/SqsProducerApplication.java
+++ src/main/java/com/msa/aws/sqs/sqs_producer/SqsProducerApplication.java
@@ -8,8 +8,14 @@ import org.springframework.context.ApplicationContext;
 public class SqsProducerApplication {
 
     public static void main(String[] args) {
+        int executionCount = 10;
+
+        if (args.length>0){
+            executionCount = Integer.parseInt(args[0]);
+        }
+
         ApplicationContext context = SpringApplication.run(SqsProducerApplication.class, args);
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < executionCount; i++){
             MessageSender sender = context.getBean(MessageSender.class);
             sender.sendMessage();
         }
